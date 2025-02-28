87005bfb2c99aa9a94d986734d8fb90dc945c44d
RajatDhasmana <rajatdhasmana@yahoo.in>
2025-02-28 14:20:51 +0530

    initial project creation commit

---
+++ NYArticles/NYArticles/NYArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYArticlesApp.swift
+//  NYArticles
+//
+//  Created by Rajat Dhasmana on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NYArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
